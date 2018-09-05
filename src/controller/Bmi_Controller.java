457f55802a1e807965a8e52b6b30ab927e886f54
Michal Szaczyk <michal.szwaczyk@outlook.com>
2018-09-05 16:30:20 +0200

    Dzialajace BMI z obrazkami

--- src/controller/Bmi_Controller.java
+++ src/controller/Bmi_Controller.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * Created $(DATE)
@@ -11,6 +13,10 @@ import javafx.scene.control.TextField;
 public class Bmi_Controller
 {
     private Main_Controller mainController;
+
+    Image niedowaga = new Image("/icon/alert.png");
+    Image prawidlowa = new Image("/icon/success.png");
+    Image nadwaga = new Image("/icon/error.png");
 
     @FXML
     RadioButton woman = new RadioButton();
@@ -26,9 +32,49 @@ public class Bmi_Controller
     Label komunikat = new Label();
 
     @FXML
+    ImageView imageOnScreen = new ImageView();
+
+    @FXML
     public void oblicz()
     {
+        komunikat.setText("");
         sprawdzPoprawnosc();
+
+        double dWaga = Double.parseDouble(waga.getText());
+        double dWzrost = Double.parseDouble(wzrost.getText()) / 100;
+        double bmiWynik;
+        dWzrost *= dWzrost;
+        bmiWynik = dWaga / dWzrost;
+
+        double min = 18;
+        double max = 25;
+
+        if(man.isSelected())
+        {
+            min = 19.3;
+            max = 26.2;
+        }
+        else if(woman.isSelected())
+        {
+            min = 15.6;
+            max = 24.7;
+        }
+
+        if(bmiWynik < min)
+        {
+            System.out.println("Masz niedowagę");
+            imageOnScreen.setImage(niedowaga);
+        }
+        else if(bmiWynik <max)
+        {
+            System.out.println("Masz prawidłową wagę");
+            imageOnScreen.setImage(prawidlowa);
+        }
+        else
+        {
+            System.out.println("Masz nadwagę");
+            imageOnScreen.setImage(nadwaga);
+        }
     }
 
     private void sprawdzPoprawnosc()
